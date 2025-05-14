be1fc61156692256a3d0706da2012ceb8edfdbdc
Rifqi Alfareza <rifqi.alfareza@gmail.com>
2025-05-14 09:21:09 +0700

    pertemuan 11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+	<h2>www.malasngoding.com</h2>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+
+</body>
+</html>
